368673760a33047171f9f14eb85487cf0e154db6
Cheng Lou <chenglou92@gmail.com>
2020-07-25 07:44:01 -0700

    Diagnosis

--- server/src/testserver.ts
+++ server/src/testserver.ts
@@ -7,21 +7,28 @@ import * as m from "vscode-jsonrpc/lib/messages";
 import * as v from "vscode-languageserver";
 import { attachPartialResult } from 'vscode-languageserver/lib/progress';
 import * as path from 'path';
-import fs from 'fs';
+import fs, { open } from 'fs';
 import * as childProcess from 'child_process';
 import { DidOpenTextDocumentNotification, DidChangeTextDocumentNotification, DidCloseTextDocumentNotification } from 'vscode-languageserver-protocol';
 import * as tmp from 'tmp';
+import { type } from 'os';
+import { Range } from 'vscode-languageserver-textdocument';
+import { strict } from 'assert';
 
 // See https://microsoft.github.io/language-server-protocol/specification Abstract Message
 // version is fixed to 2.0
-let jsonrpcVersion = '2.0'
-let bscPartialPath = path.join('node_modules', '.bin', 'bsc')
+let jsonrpcVersion = '2.0';
+let bscPartialPath = path.join('node_modules', '.bin', 'bsc');
+let bsbLogPartialPath = 'bsb.log';
+let resExt = '.res';
+let resiExt = '.resi';
 
 // https://microsoft.github.io/language-server-protocol/specification#initialize
 // According to the spec, there could be requests before the 'initialize' request. Link in comment tells how to handle them.
 let initialized = false;
 // https://microsoft.github.io/language-server-protocol/specification#exit
 let shutdownRequestAlreadyReceived = false;
+let diagnosisTimer: null | NodeJS.Timeout = null;
 
 // congrats. A simple UI problem is now a distributed system problem
 let stupidFileContentCache: { [key: string]: string } = {
@@ -53,7 +60,7 @@ let formatUsingValidBscPath = (code: string, bscPath: p.DocumentUri, isInterface
 	let result;
 	// library cleans up after itself. No need to manually remove temp file
 	let tmpobj = tmp.fileSync();
-	let extension = isInterface ? '.resi' : '.res';
+	let extension = isInterface ? resiExt : resExt;
 	let fileToFormat = tmpobj.name + extension;
 	fs.writeFileSync(fileToFormat, code, { encoding: 'utf-8' });
 	try {
@@ -70,13 +77,158 @@ let formatUsingValidBscPath = (code: string, bscPath: p.DocumentUri, isInterface
 	}
 }
 
-let startWatchingBsbOutputFile = (root: p.DocumentUri) => {
-	console.log(root)
-	// fs.watch()
+let parseBsbOutputLocation = (location: string): Range => {
+	// example bsb output location:
+	// 3:9
+	// 3:9-6:1
+	// language-server position is 0-based. Ours is 1-based. Don't forget to convert
+	let isMultiline = location.indexOf('-') >= 0
+	if (isMultiline) {
+		let [from, to] = location.split('-')
+		let [fromLine, fromChar] = from.split(':')
+		let [toLine, toChar] = to.split(':')
+		return {
+			start: { line: parseInt(fromLine) - 1, character: parseInt(fromChar) },
+			end: { line: parseInt(toLine) - 1, character: parseInt(toChar) },
+		}
+	} else {
+		let [line, char] = location.split(':')
+		let start = { line: parseInt(line) - 1, character: parseInt(char) - 1 }
+		return {
+			start: start,
+			end: start,
+		}
+	}
 }
-// let stopWatchingBsbOutputFile = () => {
-// 	fs.unwatchFile()
-// }
+type diagnosis = { range: Range, diagnosis: string }
+let parseBsbLogOutput = (content: string) => {
+	/* example bsb.log file content:
+
+Cleaning... 6 files.
+Cleaning... 87 files.
+[1/5] [34mBuilding[39m [2msrc/TestFramework.reiast[22m
+[2/5] [34mBuilding[39m [2msrc/TestFramework.reast[22m
+FAILED: src/test.cmj src/test.cmi
+
+  We've found a bug for you!
+  /Users/chenglou/github/reason-react/src/test.res 3:9
+
+  1 │ let a = 1
+  2 │ let b = "hi"
+  3 │ let a = b + 1
+
+  This has type:
+    string
+
+  But somewhere wanted:
+    int
+
+
+[15/62] [34mBuilding[39m [2msrc/ReactDOMServer.reast[22m
+	*/
+
+	// we're gonna chop that
+	let res: string[][] = [];
+	let lines = content.split('\n');
+	lines.forEach(line => {
+		if (line.startsWith('  We\'ve found a bug for you!')) {
+			res.push([])
+		} else if (/^  [0-9]+ /.test(line)) {
+			// code display. Swallow
+		} else if (line.startsWith('  ')) {
+			res[res.length - 1].push(line)
+		}
+	})
+
+	// map of file path to list of diagnosis
+	let ret: { [key: string]: diagnosis[] } = {}
+	res.forEach(diagnosisLines => {
+		let [fileAndLocation, ...diagnosisMessage] = diagnosisLines
+		let lastSpace = fileAndLocation.lastIndexOf(' ')
+		let file = fileAndLocation.substring(2, lastSpace)
+		let location = fileAndLocation.substring(lastSpace)
+		if (ret[file] == null) {
+			ret[file] = []
+		}
+		let cleanedUpDiagnosis = diagnosisMessage
+			.map(line => {
+				// remove the spaces in front
+				return line.slice(2)
+			})
+			.join('\n')
+			// remove start and end whitespaces/newlines
+			.trim();
+		ret[file].push({
+			range: parseBsbOutputLocation(location),
+			diagnosis: cleanedUpDiagnosis,
+		})
+	})
+
+	return ret
+}
+
+let startWatchingBsbOutputFile = (root: p.DocumentUri, process: NodeJS.Process) => {
+	// console.log(root);
+	// TOOD: setTimeout instead
+	let id = setInterval(() => {
+		let openFiles = Object.keys(stupidFileContentCache);
+		let bsbLogDirs: Set<p.DocumentUri> = new Set();
+		openFiles.forEach(openFile => {
+			// TODO: remove this hack
+			let filePath = openFile.replace('file://', '');
+			let bsbLogDir = findDirOfFileNearFile(bsbLogPartialPath, filePath)
+			if (bsbLogDir != null) {
+				bsbLogDirs.add(bsbLogDir);
+			}
+		});
+
+		let files: { [key: string]: diagnosis[] } = {}
+
+		let res = Array.from(bsbLogDirs)
+			.forEach(bsbLogDir => {
+				let bsbLogPath = path.join(bsbLogDir, bsbLogPartialPath);
+				let content = fs.readFileSync(bsbLogPath, { encoding: 'utf-8' });
+				let filesAndErrors = parseBsbLogOutput(content)
+				Object.keys(filesAndErrors).forEach(file => {
+					files[file] = filesAndErrors[file]
+				})
+				// content.split('\n').forEach(line => {
+				// 	let [file, diagnosis] = line.split(': ');
+
+				// 	if (files[file] == null) {
+				// 		files[file] = []
+				// 	}
+				// 	files[file].push(diagnosis)
+				// })
+			});
+
+		Object.keys(files).forEach(file => {
+			let params: p.PublishDiagnosticsParams = {
+				uri: file,
+				// there's a new optional version param from https://github.com/microsoft/language-server-protocol/issues/201
+				// not using it for now, sigh
+				diagnostics:
+					files[file].map(({ range, diagnosis }) => {
+						return {
+							range: range,
+							message: diagnosis,
+						}
+					}),
+			}
+			let notification: m.NotificationMessage = {
+				jsonrpc: jsonrpcVersion,
+				method: 'textDocument/publishDiagnostics',
+				params: params,
+			};
+			process.send!(notification);
+		})
+	}, 1000);
+
+	return id;
+}
+let stopWatchingBsbOutputFile = (timerId: NodeJS.Timeout) => {
+	clearInterval(timerId);
+}
 
 process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
 	if ((a as m.RequestMessage).id == null) {
@@ -95,15 +247,21 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
 			}
 		} else if (aa.method === DidOpenTextDocumentNotification.method) {
 			let params = (aa.params as p.DidOpenTextDocumentParams);
-			stupidFileContentCache[params.textDocument.uri] = params.textDocument.text;
+			let extName = path.extname(params.textDocument.uri)
+			if (extName === resExt || extName === resiExt) {
+				stupidFileContentCache[params.textDocument.uri] = params.textDocument.text;
+			}
 		} else if (aa.method === DidChangeTextDocumentNotification.method) {
 			let params = (aa.params as p.DidChangeTextDocumentParams);
-			let changes = params.contentChanges
-			if (changes.length === 0) {
-				// no change?
-			} else {
-				// we currently only support full changes
-				stupidFileContentCache[params.textDocument.uri] = changes[changes.length - 1].text;
+			let extName = path.extname(params.textDocument.uri)
+			if (extName === resExt || extName === resiExt) {
+				let changes = params.contentChanges
+				if (changes.length === 0) {
+					// no change?
+				} else {
+					// we currently only support full changes
+					stupidFileContentCache[params.textDocument.uri] = changes[changes.length - 1].text;
+				}
 			}
 		} else if (aa.method === DidCloseTextDocumentNotification.method) {
 			let params = (aa.params as p.DidCloseTextDocumentParams);
@@ -121,7 +279,7 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
 					message: "Server not initialized."
 				}
 			};
-			(<any>process).send(response);
+			process.send!(response);
 		} else if (aa.method === 'initialize') {
 			let param: p.InitializeParams = aa.params
 			let root = param.rootUri
@@ -129,7 +287,7 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
 				// TODO: handle single file
 				console.log("not handling single file")
 			} else {
-				startWatchingBsbOutputFile(root)
+				diagnosisTimer = startWatchingBsbOutputFile(root, process)
 			}
 			// send the list of things we support
 			let result: p.InitializeResult = {
@@ -145,16 +303,15 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
 				result: result,
 			};
 			initialized = true;
-			(<any>process).send(response);
+			process.send!(response);
 		} else if (aa.method === 'initialized') {
-			console.log('inited')
 			// sent from client after initialize. Nothing to do for now
 			let response: m.ResponseMessage = {
 				jsonrpc: jsonrpcVersion,
 				id: aa.id,
 				result: null,
 			};
-			(<any>process).send(response);
+			process.send!(response);
 		} else if (aa.method === 'shutdown') {
 			// https://microsoft.github.io/language-server-protocol/specification#shutdown
 			if (shutdownRequestAlreadyReceived) {
@@ -166,31 +323,34 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
 						message: `Language server already received the shutdown request`
 					}
 				};
-				(<any>process).send(response);
+				process.send!(response);
 			} else {
 				shutdownRequestAlreadyReceived = true
+				if (diagnosisTimer != null) {
+					stopWatchingBsbOutputFile(diagnosisTimer)
+				}
 				let response: m.ResponseMessage = {
 					jsonrpc: jsonrpcVersion,
 					id: aa.id,
 					result: null,
 				};
-				(<any>process).send(response);
+				process.send!(response);
 			}
 		} else if (aa.method === p.DocumentFormattingRequest.method) {
 			let params = (aa.params as p.DocumentFormattingParams)
 			// TODO: remove this hack
-			let filePath = params.textDocument.uri.replace('file:', '')
+			let filePath = params.textDocument.uri.replace('file://', '')
 			let extension = path.extname(params.textDocument.uri);
-			if (extension !== '.res' && extension !== '.resi') {
+			if (extension !== resExt && extension !== resiExt) {
 				let response: m.ResponseMessage = {
 					jsonrpc: jsonrpcVersion,
 					id: aa.id,
 					error: {
 						code: m.ErrorCodes.InvalidRequest,
-						message: `Not a .res or .resi file.`
+						message: `Not a ${resExt} or ${resiExt} file.`
 					}
 				};
-				(<any>process).send(response);
+				process.send!(response);
 			} else {
 				let nodeModulesParentPath = findDirOfFileNearFile(bscPartialPath, filePath)
 				if (nodeModulesParentPath == null) {
@@ -202,7 +362,7 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
 							message: `Cannot find a nearby ${bscPartialPath}. It's needed for formatting.`,
 						}
 					};
-					(<any>process).send(response);
+					process.send!(response);
 				} else {
 					// file to format potentially doesn't exist anymore because of races. But that's ok, the error from bsc should handle it
 					let code = stupidFileContentCache[params.textDocument.uri];
@@ -213,7 +373,7 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
 					let formattedResult = formatUsingValidBscPath(
 						code,
 						path.join(nodeModulesParentPath, bscPartialPath),
-						extension === '.resi',
+						extension === resiExt,
 					);
 					if (formattedResult.kind === 'success') {
 						let result: p.TextEdit[] = [{
@@ -228,7 +388,7 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
 							id: aa.id,
 							result: result,
 						};
-						(<any>process).send(response);
+						process.send!(response);
 					} else {
 						let response: m.ResponseMessage = {
 							jsonrpc: jsonrpcVersion,
@@ -238,7 +398,7 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
 								message: formattedResult.error,
 							}
 						};
-						(<any>process).send(response);
+						process.send!(response);
 					}
 				}
 			}
@@ -252,7 +412,7 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
 					message: "Unrecognized editor request."
 				}
 			};
-			(<any>process).send(response);
+			process.send!(response);
 		}
 	}
 })
