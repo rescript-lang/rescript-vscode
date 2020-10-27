632558e0a4b0f761ff8faf2b9b4b41b41fd8dce7
Iwan <mvalcke@hubspot.com>
2020-10-27 12:00:11 +0100

    Implement skeleton code for jump and hover requests

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,207 +4,234 @@ import * as t from "vscode-languageserver-types";
 import * as j from "vscode-jsonrpc";
 import * as m from "vscode-jsonrpc/lib/messages";
 import * as v from "vscode-languageserver";
-import * as path from 'path';
-import fs from 'fs';
+import * as path from "path";
+import fs from "fs";
 // TODO: check DidChangeWatchedFilesNotification.
-import { DidOpenTextDocumentNotification, DidChangeTextDocumentNotification, DidCloseTextDocumentNotification, DidChangeWatchedFilesNotification, CompletionResolveRequest } from 'vscode-languageserver-protocol';
-import * as utils from './utils';
-import * as c from './constants';
-import * as chokidar from 'chokidar'
-import { assert } from 'console';
-import { fileURLToPath } from 'url';
+import {
+  DidOpenTextDocumentNotification,
+  DidChangeTextDocumentNotification,
+  DidCloseTextDocumentNotification,
+  DidChangeWatchedFilesNotification,
+  CompletionResolveRequest,
+} from "vscode-languageserver-protocol";
+import * as utils from "./utils";
+import * as c from "./constants";
+import * as chokidar from "chokidar";
+import { assert } from "console";
+import { fileURLToPath } from "url";
 
 // https://microsoft.github.io/language-server-protocol/specification#initialize
 // According to the spec, there could be requests before the 'initialize' request. Link in comment tells how to handle them.
 let initialized = false;
 // https://microsoft.github.io/language-server-protocol/specification#exit
 let shutdownRequestAlreadyReceived = false;
-let stupidFileContentCache: Map<string, string> = new Map()
+let stupidFileContentCache: Map<string, string> = new Map();
 let projectsFiles: Map<
   string, // project root path
   {
-    openFiles: Set<string>,
-    filesWithDiagnostics: Set<string>,
-  }>
-  = new Map()
+    openFiles: Set<string>;
+    filesWithDiagnostics: Set<string>;
+  }
+> = new Map();
 // ^ caching AND states AND distributed system. Why does LSP has to be stupid like this
 
 let sendUpdatedDiagnostics = () => {
   projectsFiles.forEach(({ filesWithDiagnostics }, projectRootPath) => {
-    let content = fs.readFileSync(path.join(projectRootPath, c.compilerLogPartialPath), { encoding: 'utf-8' });
-    let { done, result: filesAndErrors } = utils.parseCompilerLogOutput(content)
+    let content = fs.readFileSync(
+      path.join(projectRootPath, c.compilerLogPartialPath),
+      { encoding: "utf-8" }
+    );
+    let { done, result: filesAndErrors } = utils.parseCompilerLogOutput(
+      content
+    );
 
     // diff
-    Object.keys(filesAndErrors).forEach(file => {
+    Object.keys(filesAndErrors).forEach((file) => {
       let params: p.PublishDiagnosticsParams = {
         uri: file,
         diagnostics: filesAndErrors[file],
-      }
+      };
       let notification: m.NotificationMessage = {
         jsonrpc: c.jsonrpcVersion,
-        method: 'textDocument/publishDiagnostics',
+        method: "textDocument/publishDiagnostics",
         params: params,
       };
       process.send!(notification);
 
-      filesWithDiagnostics.add(file)
-    })
+      filesWithDiagnostics.add(file);
+    });
     if (done) {
       // clear old files
-      filesWithDiagnostics.forEach(file => {
+      filesWithDiagnostics.forEach((file) => {
         if (filesAndErrors[file] == null) {
           // Doesn't exist in the new diagnostics. Clear this diagnostic
           let params: p.PublishDiagnosticsParams = {
             uri: file,
             diagnostics: [],
-          }
+          };
           let notification: m.NotificationMessage = {
             jsonrpc: c.jsonrpcVersion,
-            method: 'textDocument/publishDiagnostics',
+            method: "textDocument/publishDiagnostics",
             params: params,
           };
           process.send!(notification);
-          filesWithDiagnostics.delete(file)
+          filesWithDiagnostics.delete(file);
         }
-      })
+      });
     }
   });
-}
+};
 let deleteProjectDiagnostics = (projectRootPath: string) => {
-  let root = projectsFiles.get(projectRootPath)
+  let root = projectsFiles.get(projectRootPath);
   if (root != null) {
-    root.filesWithDiagnostics.forEach(file => {
+    root.filesWithDiagnostics.forEach((file) => {
       let params: p.PublishDiagnosticsParams = {
         uri: file,
         diagnostics: [],
-      }
+      };
       let notification: m.NotificationMessage = {
         jsonrpc: c.jsonrpcVersion,
-        method: 'textDocument/publishDiagnostics',
+        method: "textDocument/publishDiagnostics",
         params: params,
       };
       process.send!(notification);
-    })
+    });
 
-    projectsFiles.delete(projectRootPath)
+    projectsFiles.delete(projectRootPath);
   }
-}
+};
 
-let compilerLogsWatcher = chokidar.watch([])
-  .on('all', (_e, changedPath) => {
-    sendUpdatedDiagnostics()
-  })
+let compilerLogsWatcher = chokidar.watch([]).on("all", (_e, changedPath) => {
+  sendUpdatedDiagnostics();
+});
 let stopWatchingCompilerLog = () => {
   // TODO: cleanup of compilerLogs?
-  compilerLogsWatcher.close()
-}
+  compilerLogsWatcher.close();
+};
 
 let openedFile = (fileUri: string, fileContent: string) => {
-  let filePath = fileURLToPath(fileUri)
+  let filePath = fileURLToPath(fileUri);
 
-  stupidFileContentCache.set(filePath, fileContent)
+  stupidFileContentCache.set(filePath, fileContent);
 
-  let projectRootPath = utils.findProjectRootOfFile(filePath)
+  let projectRootPath = utils.findProjectRootOfFile(filePath);
   if (projectRootPath != null) {
     if (!projectsFiles.has(projectRootPath)) {
-      projectsFiles.set(projectRootPath, { openFiles: new Set(), filesWithDiagnostics: new Set() })
-      compilerLogsWatcher.add(path.join(projectRootPath, c.compilerLogPartialPath))
+      projectsFiles.set(projectRootPath, {
+        openFiles: new Set(),
+        filesWithDiagnostics: new Set(),
+      });
+      compilerLogsWatcher.add(
+        path.join(projectRootPath, c.compilerLogPartialPath)
+      );
     }
-    let root = projectsFiles.get(projectRootPath)!
-    root.openFiles.add(filePath)
+    let root = projectsFiles.get(projectRootPath)!;
+    root.openFiles.add(filePath);
     // no need to call sendUpdatedDiagnostics() here; the watcher add will
     // call the listener which calls it
   }
-}
+};
 let closedFile = (fileUri: string) => {
-  let filePath = fileURLToPath(fileUri)
+  let filePath = fileURLToPath(fileUri);
 
-  stupidFileContentCache.delete(filePath)
+  stupidFileContentCache.delete(filePath);
 
-  let projectRootPath = utils.findProjectRootOfFile(filePath)
+  let projectRootPath = utils.findProjectRootOfFile(filePath);
   if (projectRootPath != null) {
-    let root = projectsFiles.get(projectRootPath)
+    let root = projectsFiles.get(projectRootPath);
     if (root != null) {
-      root.openFiles.delete(filePath)
+      root.openFiles.delete(filePath);
       // clear diagnostics too if no open files open in said project
       if (root.openFiles.size === 0) {
-        compilerLogsWatcher.unwatch(path.join(projectRootPath, c.compilerLogPartialPath))
-        deleteProjectDiagnostics(projectRootPath)
+        compilerLogsWatcher.unwatch(
+          path.join(projectRootPath, c.compilerLogPartialPath)
+        );
+        deleteProjectDiagnostics(projectRootPath);
       }
     }
   }
-}
+};
 let updateOpenedFile = (fileUri: string, fileContent: string) => {
-  let filePath = fileURLToPath(fileUri)
-  assert(stupidFileContentCache.has(filePath))
-  stupidFileContentCache.set(filePath, fileContent)
-}
+  let filePath = fileURLToPath(fileUri);
+  assert(stupidFileContentCache.has(filePath));
+  stupidFileContentCache.set(filePath, fileContent);
+};
 let getOpenedFileContent = (fileUri: string) => {
-  let filePath = fileURLToPath(fileUri)
-  let content = stupidFileContentCache.get(filePath)!
-  assert(content != null)
-  return content
-}
+  let filePath = fileURLToPath(fileUri);
+  let content = stupidFileContentCache.get(filePath)!;
+  assert(content != null);
+  return content;
+};
 
-process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
+process.on("message", (a: m.RequestMessage | m.NotificationMessage) => {
+  console.log(a);
+  console.log("yo");
   if ((a as m.RequestMessage).id == null) {
     // this is a notification message, aka the client ends it and doesn't want a reply
-    let aa = (a as m.NotificationMessage)
-    if (!initialized && aa.method !== 'exit') {
+    let aa = a as m.NotificationMessage;
+    if (!initialized && aa.method !== "exit") {
       // From spec: "Notifications should be dropped, except for the exit notification. This will allow the exit of a server without an initialize request"
       // For us: do nothing. We don't have anything we need to clean up right now
       // TODO: we might have things we need to clean up now... like some watcher stuff
-    } else if (aa.method === 'exit') {
+    } else if (aa.method === "exit") {
       // The server should exit with success code 0 if the shutdown request has been received before; otherwise with error code 1
       if (shutdownRequestAlreadyReceived) {
-        process.exit(0)
+        process.exit(0);
       } else {
-        process.exit(1)
+        process.exit(1);
       }
     } else if (aa.method === DidOpenTextDocumentNotification.method) {
-      let params = (aa.params as p.DidOpenTextDocumentParams);
-      let extName = path.extname(params.textDocument.uri)
+      let params = aa.params as p.DidOpenTextDocumentParams;
+      let extName = path.extname(params.textDocument.uri);
       if (extName === c.resExt || extName === c.resiExt) {
-        openedFile(params.textDocument.uri, params.textDocument.text)
+        openedFile(params.textDocument.uri, params.textDocument.text);
       }
     } else if (aa.method === DidChangeTextDocumentNotification.method) {
-      let params = (aa.params as p.DidChangeTextDocumentParams);
-      let extName = path.extname(params.textDocument.uri)
+      let params = aa.params as p.DidChangeTextDocumentParams;
+      let extName = path.extname(params.textDocument.uri);
       if (extName === c.resExt || extName === c.resiExt) {
-        let changes = params.contentChanges
+        let changes = params.contentChanges;
         if (changes.length === 0) {
           // no change?
         } else {
           // we currently only support full changes
-          updateOpenedFile(params.textDocument.uri, changes[changes.length - 1].text)
+          updateOpenedFile(
+            params.textDocument.uri,
+            changes[changes.length - 1].text
+          );
         }
       }
     } else if (aa.method === DidCloseTextDocumentNotification.method) {
-      let params = (aa.params as p.DidCloseTextDocumentParams);
-      closedFile(params.textDocument.uri)
+      let params = aa.params as p.DidCloseTextDocumentParams;
+      closedFile(params.textDocument.uri);
     }
   } else {
     // this is a request message, aka client sent request and waits for our mandatory reply
-    let aa = (a as m.RequestMessage)
-    if (!initialized && aa.method !== 'initialize') {
+    let aa = a as m.RequestMessage;
+    if (!initialized && aa.method !== "initialize") {
       let response: m.ResponseMessage = {
         jsonrpc: c.jsonrpcVersion,
         id: aa.id,
         error: {
           code: m.ErrorCodes.ServerNotInitialized,
-          message: "Server not initialized."
-        }
+          message: "Server not initialized.",
+        },
       };
       process.send!(response);
-    } else if (aa.method === 'initialize') {
+    } else if (aa.method === "initialize") {
       // send the list of features we support
       let result: p.InitializeResult = {
+        // This tells the client: "hey, we support the following operations".
+        // Example: we want to expose "jump-to-definition".
+        // By adding `definitionProvider: true`, the client will now send "jump-to-definition" requests.
         capabilities: {
           // TODO: incremental sync?
           textDocumentSync: v.TextDocumentSyncKind.Full,
           documentFormattingProvider: true,
-        }
-      }
+          hoverProvider: true,
+          definitionProvider: true,
+        },
+      };
       let response: m.ResponseMessage = {
         jsonrpc: c.jsonrpcVersion,
         id: aa.id,
@@ -212,7 +239,7 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
       };
       initialized = true;
       process.send!(response);
-    } else if (aa.method === 'initialized') {
+    } else if (aa.method === "initialized") {
       // sent from client after initialize. Nothing to do for now
       let response: m.ResponseMessage = {
         jsonrpc: c.jsonrpcVersion,
@@ -220,7 +247,7 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
         result: null,
       };
       process.send!(response);
-    } else if (aa.method === 'shutdown') {
+    } else if (aa.method === "shutdown") {
       // https://microsoft.github.io/language-server-protocol/specification#shutdown
       if (shutdownRequestAlreadyReceived) {
         let response: m.ResponseMessage = {
@@ -228,14 +255,14 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
           id: aa.id,
           error: {
             code: m.ErrorCodes.InvalidRequest,
-            message: `Language server already received the shutdown request`
-          }
+            message: `Language server already received the shutdown request`,
+          },
         };
         process.send!(response);
       } else {
-        shutdownRequestAlreadyReceived = true
+        shutdownRequestAlreadyReceived = true;
         // TODO: recheck logic around init/shutdown...
-        stopWatchingCompilerLog()
+        stopWatchingCompilerLog();
 
         let response: m.ResponseMessage = {
           jsonrpc: c.jsonrpcVersion,
@@ -244,6 +271,33 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
         };
         process.send!(response);
       }
+    } else if (aa.method === p.HoverRequest.method) {
+      let dummyHoverResponse: m.ResponseMessage = {
+        jsonrpc: c.jsonrpcVersion,
+        id: aa.id,
+        // type result = Hover | null
+        // type Hover = {contents: MarkedString | MarkedString[] | MarkupContent, range?: Range}
+        result: { contents: "Time to go for a 20k run!" },
+      };
+
+      process.send!(dummyHoverResponse);
+    } else if (aa.method === p.DefinitionRequest.method) {
+      // https://microsoft.github.io/language-server-protocol/specifications/specification-current/#textDocument_definition
+      let dummyDefinitionResponse: m.ResponseMessage = {
+        jsonrpc: c.jsonrpcVersion,
+        id: aa.id,
+        // result should be: Location | Array<Location> | Array<LocationLink> | null
+        result: {
+          uri: aa.params.textDocument.uri,
+          range: {
+            start: { line: 2, character: 4 },
+            end: { line: 2, character: 12 },
+          },
+        },
+        // error: code and message set in case an exception happens during the definition request.
+      };
+
+      process.send!(dummyDefinitionResponse);
     } else if (aa.method === p.DocumentFormattingRequest.method) {
       // technically, a formatting failure should reply with the error. Sadly
       // the LSP alert box for these error replies sucks (e.g. doesn't actually
@@ -256,67 +310,72 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
         jsonrpc: c.jsonrpcVersion,
         id: aa.id,
         result: [],
-      }
+      };
 
-      let params = (aa.params as p.DocumentFormattingParams)
-      let filePath = fileURLToPath(params.textDocument.uri)
+      let params = aa.params as p.DocumentFormattingParams;
+      let filePath = fileURLToPath(params.textDocument.uri);
       let extension = path.extname(params.textDocument.uri);
       if (extension !== c.resExt && extension !== c.resiExt) {
         let params: p.ShowMessageParams = {
           type: p.MessageType.Error,
-          message: `Not a ${c.resExt} or ${c.resiExt} file. Cannot format it.`
-        }
+          message: `Not a ${c.resExt} or ${c.resiExt} file. Cannot format it.`,
+        };
         let response: m.NotificationMessage = {
           jsonrpc: c.jsonrpcVersion,
-          method: 'window/showMessage',
+          method: "window/showMessage",
           params: params,
-        }
+        };
         process.send!(fakeSuccessResponse);
         process.send!(response);
       } else {
-        let projectRootPath = utils.findProjectRootOfFile(filePath)
+        let projectRootPath = utils.findProjectRootOfFile(filePath);
         if (projectRootPath == null) {
           let params: p.ShowMessageParams = {
             type: p.MessageType.Error,
             message: `Cannot find a nearby ${c.bsconfigPartialPath}. It's needed for determining the project's root.`,
-          }
+          };
           let response: m.NotificationMessage = {
             jsonrpc: c.jsonrpcVersion,
-            method: 'window/showMessage',
+            method: "window/showMessage",
             params: params,
-          }
+          };
           process.send!(fakeSuccessResponse);
           process.send!(response);
         } else {
-          let bscPath = path.join(projectRootPath, c.bscPartialPath)
+          let bscPath = path.join(projectRootPath, c.bscPartialPath);
           if (!fs.existsSync(bscPath)) {
             let params: p.ShowMessageParams = {
               type: p.MessageType.Error,
               message: `Cannot find a nearby ${c.bscPartialPath}. It's needed for formatting.`,
-            }
+            };
             let response: m.NotificationMessage = {
               jsonrpc: c.jsonrpcVersion,
-              method: 'window/showMessage',
+              method: "window/showMessage",
               params: params,
-            }
+            };
             process.send!(fakeSuccessResponse);
             process.send!(response);
           } else {
             // code will always be defined here, even though technically it can be undefined
-            let code = getOpenedFileContent(params.textDocument.uri)
+            let code = getOpenedFileContent(params.textDocument.uri);
             let formattedResult = utils.formatUsingValidBscPath(
               code,
               bscPath,
-              extension === c.resiExt,
+              extension === c.resiExt
             );
-            if (formattedResult.kind === 'success') {
-              let result: p.TextEdit[] = [{
-                range: {
-                  start: { line: 0, character: 0 },
-                  end: { line: Number.MAX_VALUE, character: Number.MAX_VALUE }
+            if (formattedResult.kind === "success") {
+              let result: p.TextEdit[] = [
+                {
+                  range: {
+                    start: { line: 0, character: 0 },
+                    end: {
+                      line: Number.MAX_VALUE,
+                      character: Number.MAX_VALUE,
+                    },
+                  },
+                  newText: formattedResult.result,
                 },
-                newText: formattedResult.result,
-              }]
+              ];
               let response: m.ResponseMessage = {
                 jsonrpc: c.jsonrpcVersion,
                 id: aa.id,
@@ -333,17 +392,16 @@ process.on('message', (a: (m.RequestMessage | m.NotificationMessage)) => {
           }
         }
       }
-
     } else {
       let response: m.ResponseMessage = {
         jsonrpc: c.jsonrpcVersion,
         id: aa.id,
         error: {
           code: m.ErrorCodes.InvalidRequest,
-          message: "Unrecognized editor request."
-        }
+          message: "Unrecognized editor request.",
+        },
       };
       process.send!(response);
     }
   }
-})
+});
