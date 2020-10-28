73062399f994eb8d383ce9a4679e90ca8c100541
Cheng Lou <chenglou92@gmail.com>
2020-10-28 07:35:45 -0700

    Format

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -1,59 +1,75 @@
-import { Range } from 'vscode-languageserver-textdocument';
-import * as c from './constants';
-import * as childProcess from 'child_process';
+import { Range } from "vscode-languageserver-textdocument";
+import * as c from "./constants";
+import * as childProcess from "child_process";
 import * as p from "vscode-languageserver-protocol";
-import * as path from 'path';
+import * as path from "path";
 import * as t from "vscode-languageserver-types";
-import * as tmp from 'tmp';
-import fs from 'fs';
-import { report } from 'process';
+import * as tmp from "tmp";
+import fs from "fs";
 
-// TODO: races here
+// TODO: races here?
 // TODO: this doesn't handle file:/// scheme
-export let findProjectRootOfFile = (source: p.DocumentUri): null | p.DocumentUri => {
-	let dir = path.dirname(source)
+export let findProjectRootOfFile = (
+	source: p.DocumentUri
+): null | p.DocumentUri => {
+	let dir = path.dirname(source);
 	if (fs.existsSync(path.join(dir, c.bsconfigPartialPath))) {
-		return dir
+		return dir;
 	} else {
 		if (dir === source) {
 			// reached top
-			return null
+			return null;
 		} else {
-			return findProjectRootOfFile(dir)
+			return findProjectRootOfFile(dir);
 		}
 	}
-}
-
-type execResult = {
-	kind: 'success',
-	result: string
-} | {
-	kind: 'error'
-	error: string,
 };
-export let formatUsingValidBscPath = (code: string, bscPath: p.DocumentUri, isInterface: boolean): execResult => {
+
+type execResult =
+	| {
+		kind: "success";
+		result: string;
+	}
+	| {
+		kind: "error";
+		error: string;
+	};
+export let formatUsingValidBscPath = (
+	code: string,
+	bscPath: p.DocumentUri,
+	isInterface: boolean
+): execResult => {
 	// library cleans up after itself. No need to manually remove temp file
 	let tmpobj = tmp.fileSync();
 	let extension = isInterface ? c.resiExt : c.resExt;
 	let fileToFormat = tmpobj.name + extension;
-	fs.writeFileSync(fileToFormat, code, { encoding: 'utf-8' });
+	fs.writeFileSync(fileToFormat, code, { encoding: "utf-8" });
 	try {
-		let result = childProcess.execFileSync(bscPath, ['-color', 'never', '-format', fileToFormat], { stdio: 'pipe' })
+		let result = childProcess.execFileSync(
+			bscPath,
+			["-color", "never", "-format", fileToFormat],
+			{ stdio: "pipe" }
+		);
 		return {
-			kind: 'success',
+			kind: "success",
 			result: result.toString(),
-		}
+		};
 	} catch (e) {
 		return {
-			kind: 'error',
+			kind: "error",
 			error: e.message,
-		}
+		};
 	}
-}
+};
 
-export let runBsbWatcherUsingValidBsbPath = (bsbPath: p.DocumentUri, projectRootPath: p.DocumentUri) => {
-	let process = childProcess.execFile(bsbPath, ['-w'], { cwd: projectRootPath })
-	return process
+export let runBsbWatcherUsingValidBsbPath = (
+	bsbPath: p.DocumentUri,
+	projectRootPath: p.DocumentUri
+) => {
+	let process = childProcess.execFile(bsbPath, ["-w"], {
+		cwd: projectRootPath,
+	});
+	return process;
 	// try {
 	// 	let result = childProcess.execFileSync(bsbPath, [], { stdio: 'pipe', cwd: projectRootPath })
 	// 	return {
@@ -66,7 +82,7 @@ export let runBsbWatcherUsingValidBsbPath = (bsbPath: p.DocumentUri, projectRoot
 	// 		error: e.message,
 	// 	}
 	// }
-}
+};
 
 export let parseDiagnosticLocation = (location: string): Range => {
 	// example output location:
@@ -76,34 +92,39 @@ export let parseDiagnosticLocation = (location: string): Range => {
 
 	// language-server position is 0-based. Ours is 1-based. Don't forget to convert
 	// also, our end character is inclusive. Language-server's is exclusive
-	let isRange = location.indexOf('-') >= 0
+	let isRange = location.indexOf("-") >= 0;
 	if (isRange) {
-		let [from, to] = location.split('-')
-		let [fromLine, fromChar] = from.split(':')
-		let isSingleLine = to.indexOf(':') >= 0
-		let [toLine, toChar] = isSingleLine ? to.split(':') : [fromLine, to]
+		let [from, to] = location.split("-");
+		let [fromLine, fromChar] = from.split(":");
+		let isSingleLine = to.indexOf(":") >= 0;
+		let [toLine, toChar] = isSingleLine ? to.split(":") : [fromLine, to];
 		return {
-			start: { line: parseInt(fromLine) - 1, character: parseInt(fromChar) - 1 },
+			start: {
+				line: parseInt(fromLine) - 1,
+				character: parseInt(fromChar) - 1,
+			},
 			end: { line: parseInt(toLine) - 1, character: parseInt(toChar) },
-		}
+		};
 	} else {
-		let [line, char] = location.split(':')
-		let start = { line: parseInt(line) - 1, character: parseInt(char) }
+		let [line, char] = location.split(":");
+		let start = { line: parseInt(line) - 1, character: parseInt(char) };
 		return {
 			start: start,
 			end: start,
-		}
+		};
 	}
-}
+};
 
 type filesDiagnostics = {
 	[key: string]: p.Diagnostic[];
-}
+};
 type parsedCompilerLogResult = {
-	done: boolean,
-	result: filesDiagnostics,
-}
-export let parseCompilerLogOutput = (content: string): parsedCompilerLogResult => {
+	done: boolean;
+	result: filesDiagnostics;
+};
+export let parseCompilerLogOutput = (
+	content: string
+): parsedCompilerLogResult => {
 	/* example .compiler.log file content that we're gonna parse:
 
 #Start(1600519680823)
@@ -145,27 +166,27 @@ export let parseCompilerLogOutput = (content: string): parsedCompilerLogResult =
 	*/
 
 	type parsedDiagnostic = {
-		code: number | undefined,
-		severity: t.DiagnosticSeverity,
-		tag: t.DiagnosticTag | undefined,
-		content: string[]
-	}
+		code: number | undefined;
+		severity: t.DiagnosticSeverity;
+		tag: t.DiagnosticTag | undefined;
+		content: string[];
+	};
 	let parsedDiagnostics: parsedDiagnostic[] = [];
-	let lines = content.split('\n');
+	let lines = content.split("\n");
 	let done = false;
 
 	for (let i = 0; i < lines.length; i++) {
 		let line = lines[i];
-		if (line.startsWith('  We\'ve found a bug for you!')) {
+		if (line.startsWith("  We've found a bug for you!")) {
 			parsedDiagnostics.push({
 				code: undefined,
 				severity: t.DiagnosticSeverity.Error,
 				tag: undefined,
-				content: []
-			})
-		} else if (line.startsWith('  Warning number ')) {
-			let warningNumber = parseInt(line.slice('  Warning number '.length))
-			let tag: t.DiagnosticTag | undefined = undefined
+				content: [],
+			});
+		} else if (line.startsWith("  Warning number ")) {
+			let warningNumber = parseInt(line.slice("  Warning number ".length));
+			let tag: t.DiagnosticTag | undefined = undefined;
 			switch (warningNumber) {
 				case 11:
 				case 20:
@@ -183,51 +204,52 @@ export let parseCompilerLogOutput = (content: string): parsedCompilerLogResult =
 				case 66:
 				case 67:
 				case 101:
-					tag = t.DiagnosticTag.Unnecessary
+					tag = t.DiagnosticTag.Unnecessary;
 					break;
 				case 3:
-					tag = t.DiagnosticTag.Deprecated
+					tag = t.DiagnosticTag.Deprecated;
 					break;
 			}
 			parsedDiagnostics.push({
 				code: Number.isNaN(warningNumber) ? undefined : warningNumber,
 				severity: t.DiagnosticSeverity.Warning,
 				tag: tag,
-				content: []
-			})
-		} else if (line.startsWith('  Syntax error!')) {
+				content: [],
+			});
+		} else if (line.startsWith("  Syntax error!")) {
 			parsedDiagnostics.push({
 				code: undefined,
 				severity: t.DiagnosticSeverity.Error,
 				tag: undefined,
-				content: []
-			})
-		} else if (line.startsWith('#Done(')) {
-			done = true
+				content: [],
+			});
+		} else if (line.startsWith("#Done(")) {
+			done = true;
 		} else if (/^  +[0-9]+ /.test(line)) {
 			// code display. Swallow
-		} else if (line.startsWith('  ')) {
-			parsedDiagnostics[parsedDiagnostics.length - 1].content.push(line)
+		} else if (line.startsWith("  ")) {
+			parsedDiagnostics[parsedDiagnostics.length - 1].content.push(line);
 		}
 	}
 
-	let result: filesDiagnostics = {}
-	parsedDiagnostics.forEach(parsedDiagnostic => {
-		let [fileAndLocation, ...diagnosticMessage] = parsedDiagnostic.content
-		let locationSeparator = fileAndLocation.indexOf(':')
-		let file = fileAndLocation.substring(2, locationSeparator)
-		let location = fileAndLocation.substring(locationSeparator + 1)
+	let result: filesDiagnostics = {};
+	parsedDiagnostics.forEach((parsedDiagnostic) => {
+		let [fileAndLocation, ...diagnosticMessage] = parsedDiagnostic.content;
+		let locationSeparator = fileAndLocation.indexOf(":");
+		let file = fileAndLocation.substring(2, locationSeparator);
+		let location = fileAndLocation.substring(locationSeparator + 1);
 		if (result[file] == null) {
-			result[file] = []
+			result[file] = [];
 		}
-		let cleanedUpDiagnostic = diagnosticMessage
-			.map(line => {
-				// remove the spaces in front
-				return line.slice(2)
-			})
-			.join('\n')
-			// remove start and end whitespaces/newlines
-			.trim() + '\n';
+		let cleanedUpDiagnostic =
+			diagnosticMessage
+				.map((line) => {
+					// remove the spaces in front
+					return line.slice(2);
+				})
+				.join("\n")
+				// remove start and end whitespaces/newlines
+				.trim() + "\n";
 		result[file].push({
 			severity: parsedDiagnostic.severity,
 			tags: parsedDiagnostic.tag === undefined ? [] : [parsedDiagnostic.tag],
@@ -235,8 +257,8 @@ export let parseCompilerLogOutput = (content: string): parsedCompilerLogResult =
 			range: parseDiagnosticLocation(location),
 			source: "ReScript",
 			message: cleanedUpDiagnostic,
-		})
-	})
+		});
+	});
 
-	return { done, result }
-}
+	return { done, result };
+};
