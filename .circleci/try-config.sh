4c6f2c6d5f99134bfea7a9023aaf5bff018cba3c
Cristiano Calcagno <ccrisccris@gmail.com>
2020-11-18 11:10:22 +0100

    Add circleci config.

--- .circleci/try-config.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-curl --user `cat ~/.ssh/circle-token`: \
-    --request POST \
-    --form revision=c1bd97a7e136cd54f6ec00048776f5badaa361a6\
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/jaredly/reason-language-server/tree/master
-
