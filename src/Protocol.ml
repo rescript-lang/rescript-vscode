4b9ef3590d3f9b4923be640672b5a70a1ae1848a
Cheng Lou <chenglou92@gmail.com>
2021-04-09 05:53:01 -0700

    Clean up hover code
    Pull out hover

--- src/Protocol.ml
+++ src/Protocol.ml
@@ -1,15 +1,10 @@
-let posOfLexing {Lexing.pos_lnum; pos_cnum; pos_bol} =
-  Json.Object [("line", Json.Number (float_of_int (pos_lnum - 1))); ("character", Json.Number (float_of_int (pos_cnum - pos_bol)))]
-
-let rangeOfLoc {Location.loc_start; loc_end} =
-  Json.Object [("start", posOfLexing loc_start); ("end", posOfLexing loc_end)]
-
 let array l = "[" ^ (String.concat ", " l) ^ "]"
 
 type markupContent = {
   kind: string;
   value: string;
 }
+
 type completionItem = {
   label: string;
   kind: int;
@@ -17,6 +12,11 @@ type completionItem = {
   detail: string;
   documentation: markupContent;
 }
+
+type hover = {
+  contents: string;
+}
+
 let stringifyMarkupContent (m: markupContent) =
   Printf.sprintf {|{"kind": "%s", "value": "%s"}|}
   m.kind (String.escaped m.value)
@@ -34,3 +34,9 @@ let stringifyCompletionItem c =
   (c.tags |> List.map string_of_int |> array)
   (String.escaped c.detail)
   (stringifyMarkupContent c.documentation)
+
+let stringifyHover h =
+  Printf.sprintf {|{"contents": "%s"}|}
+  (String.escaped h.contents)
+
+let null = "null"
