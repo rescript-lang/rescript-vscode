f4b7c719a0225d7766c6e914021c9ef6b77ca4a0
Cristiano Calcagno <ccrisccris@gmail.com>
2021-04-14 06:26:24 +0200

    Try to add an autocomplete test.

--- test.sh
+++ test.sh
@@ -2,3 +2,7 @@
 
 ./_build/install/default/bin/rescript-editor-support.exe definition test/src/Definition.res 2 8
 
+
+cat test/src/Complete.res test/src/Complete.append >test/src/Complete.current	
+./_build/install/default/bin/rescript-editor-support.exe complete test/src/Complete.res 1 6 test/src/Complete.current
+
