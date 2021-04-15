cf3241a5fed7928b51e9343683fc2ea799277f9d
Cristiano Calcagno <ccrisccris@gmail.com>
2021-04-15 07:05:56 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-./_build/install/default/bin/rescript-editor-support.exe -test definition test/src/Definition.res 2 8
+./_build/install/default/bin/rescript-editor-support.exe test test/src/Definition.res
 
 ./_build/install/default/bin/rescript-editor-support.exe test test/src/Complete.res
