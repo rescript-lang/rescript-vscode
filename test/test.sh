1a68f9d8f7ab3c476c760f83006e81643c99ab96
Cristiano Calcagno <ccrisccris@gmail.com>
2021-04-16 04:55:28 +0200

    Add test command to package.json.

--- test/test.sh
+++ test/test.sh
@@ -4,14 +4,7 @@ function exp {
   echo "$(dirname $1)/expected/$(basename $1).txt"
 }
 
-taskCount=0
-function maybeWait {
-  let taskCount+=1
-  # spawn in batch of 20 processes
-  [[ $((taskCount % 20)) = 0 ]] && wait
-}
-
 ./node_modules/.bin/rescript build
 for file in src/**/*.res; do
-  ../_build/install/default/bin/rescript-editor-support.exe test $file &> $(exp $file) & maybeWait
+  ../_build/install/default/bin/rescript-editor-support.exe test $file &> $(exp $file)
 done
