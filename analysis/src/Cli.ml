f5c97aa23b21f1ae23aa3d8585b06b8a11d5ee7e
Cristiano Calcagno <ccrisccris@gmail.com>
2021-04-27 03:06:41 +0200

    Update cmdline -hlep
    Fixes https://github.com/rescript-lang/rescript-vscode/issues/124

--- analysis/src/Cli.ml
+++ analysis/src/Cli.ml
@@ -3,55 +3,62 @@ let help =
 **Private CLI For rescript-vscode usage only**
 
 Examples:
-  ./run.exe dump src/MyFile.res src/MyFile2.res
   ./run.exe complete src/MyFile.res 0 4 currentContent.res
-  ./run.exe hover src/MyFile.res 10 2
   ./run.exe definition src/MyFile.res 9 3
+  ./run.exe dump src/MyFile.res src/MyFile2.res
+  ./run.exe documentSymbol src/Foo.res 
+  ./run.exe hover src/MyFile.res 10 2
+  ./run.exe references src/MyFile.res 10 2
+  ./run.exe test src/MyFile.res
 
 Note: coordinates are zero-based, so the first position is 0 0.
 
 Options:
-  dump: debugging. definition and hover for Foo.res and Foo2.res:
+  complete: compute autocomplete for MyFile.res at line 0 and column 4,
+    where MyFile.res is being edited and the editor content is in file current.res.
 
-    ./run.exe dump src/Foo.res src/Foo2.res
+    ./run.exe complete src/MyFile.res 0 4 current.res
 
-  complete: compute autocomplete for Foo.res at line 0 and column 4,
-    where Foo.res is being edited and the editor content is in file current.res.
+  definition: get definition for item in MyFile.res at line 10 column 2:
 
-    ./run.exe complete src/Foo.res 0 4 current.res
+    ./run.exe definition src/MyFile.res 10 2
 
-  hover: get inferred type for Foo.res at line 10 column 2:
+  dump: for debugging, show all definitions and hovers for MyFile.res and MyFile.res:
 
-    ./run.exe hover src/Foo.res 10 2
+    ./run.exe dump src/Foo.res src/MyFile.res
 
-  definition: get definition for item in Foo.res at line 10 column 2:
+  documentSymbol: get all symbols declared in MyFile.res
 
-    ./run.exe definition src/Foo.res 10 2
+    ./run.exe documentSymbol src/MyFile.res 
 
-  references: get references to item in Foo.res at line 10 column 2:
+  hover: get inferred type for MyFile.res at line 10 column 2:
 
-    ./run.exe references src/Foo.res 10 2
+    ./run.exe hover src/MyFile.res 10 2
 
-  documentSymbol: get all symbols in Foo.res:
+  references: get all references to item in MyFile.res at line 10 column 2:
 
-    ./run.exe documentSymbol src/Foo.res 
-  |}
+    ./run.exe references src/MyFile.res 10 2
+
+  test: run tests specified by special comments in file src/MyFile.res
+
+  ./run.exe test src/src/MyFile.res
+|}
 
 let main () =
   match Array.to_list Sys.argv with
   | [_; "complete"; path; line; col; currentFile] ->
     Commands.complete ~path ~line:(int_of_string line) ~col:(int_of_string col)
       ~currentFile
-  | [_; "hover"; path; line; col] ->
-    Commands.hover ~path ~line:(int_of_string line) ~col:(int_of_string col)
   | [_; "definition"; path; line; col] ->
     Commands.definition ~path ~line:(int_of_string line)
       ~col:(int_of_string col)
+  | _ :: "dump" :: files -> Commands.dump files
+  | [_; "documentSymbol"; path] -> Commands.documentSymbol ~path
+  | [_; "hover"; path; line; col] ->
+    Commands.hover ~path ~line:(int_of_string line) ~col:(int_of_string col)
   | [_; "references"; path; line; col] ->
     Commands.references ~path ~line:(int_of_string line)
       ~col:(int_of_string col)
-  | [_; "documentSymbol"; path] -> Commands.documentSymbol ~path
-  | _ :: "dump" :: files -> Commands.dump files
   | [_; "test"; path] -> Commands.test ~path
   | args when List.mem "-h" args || List.mem "--help" args -> prerr_endline help
   | _ ->
