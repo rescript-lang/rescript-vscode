a222edb91b1c76c80af8618957fd1e1afb50dde4
Cheng Lou <chenglou92@gmail.com>
2021-04-28 07:01:23 -0700

    Git diff earlier to debug

--- analysis/test.sh
+++ analysis/test.sh
@@ -2,9 +2,11 @@ function exp {
   echo "$(dirname $1)/expected/$(basename $1).txt"
 }
 
-node ./checkErrors.js
+echo "git status right now"
+git --no-pager diff --word-diff-regex=. tests/src/expected/Auto.res.txt
+echo "done"
 
-hexdump -C tests/src/expected/Auto.res.txt > old.hex
+node ./checkErrors.js
 
 for file in tests/src/*.{res,resi}; do
   ./rescript-editor-analysis.exe test $file &> $(exp $file)
@@ -21,9 +23,5 @@ else
   printf "${warningYellow}⚠️ There are unstaged differences in tests/! Did you break a test?\n${diff}\n${reset}"
   node ./checkErrors.js
   git --no-pager diff --word-diff-regex=. tests/src/expected/Auto.res.txt
-  echo "diff hex:"
-  hexdump -C tests/src/expected/Auto.res.txt > new.hex
-  diff old.hex new.hex
-  echo "done"
   exit 1
 fi
