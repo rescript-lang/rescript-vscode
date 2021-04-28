ba3ef0a5482956ad04df542cbadd26a93ea0a579
Cheng Lou <chenglou92@gmail.com>
2021-04-28 07:40:55 -0700

    Last cats

--- analysis/test.sh
+++ analysis/test.sh
@@ -6,7 +6,7 @@ echo "cat -A test.sh"
 cat -A tests/src/expected/Auto.res.txt
 echo "done---------"
 
-node ./checkErrors.js
+# node ./checkErrors.js
 
 for file in tests/src/*.{res,resi}; do
   ./rescript-editor-analysis.exe test $file &> $(exp $file)
@@ -25,11 +25,17 @@ successGreen='\033[0;32m'
 reset='\033[0m'
 
 diff=$(git ls-files --modified tests/src/expected)
+echo "cat -A test.sh third time (after ls-files)"
+cat -A tests/src/expected/Auto.res.txt
+echo "done 3---------"
 if [[ $diff = "" ]]; then
   printf "${successGreen}✅ No unstaged tests difference.${reset}\n"
 else
   printf "${warningYellow}⚠️ There are unstaged differences in tests/! Did you break a test?\n${diff}\n${reset}"
-  node ./checkErrors.js
+  # node ./checkErrors.js
   git --no-pager diff --word-diff-regex=. tests/src/expected/Auto.res.txt
+  echo "cat -A test.sh last time (after git diff)"
+  cat -A tests/src/expected/Auto.res.txt
+  echo "done 4last---------"
   exit 1
 fi
