ddb553f9b5e7df915159a148d9ea8d8a5efc8ed6
Cheng Lou <chenglou92@gmail.com>
2021-04-27 23:20:21 -0700

    What about trying hover directly instead of test

--- analysis/test.sh
+++ analysis/test.sh
@@ -1,20 +1,22 @@
-function exp {
-  echo "$(dirname $1)/expected/$(basename $1).txt"
-}
+# function exp {
+#   echo "$(dirname $1)/expected/$(basename $1).txt"
+# }
 
-for file in tests/src/*.{res,resi}; do
-  ./rescript-editor-analysis.exe test $file &> $(exp $file)
-done
+# for file in tests/src/*.{res,resi}; do
+#   ./rescript-editor-analysis.exe test $file &> $(exp $file)
+# done
 
-warningYellow='\033[0;33m'
-successGreen='\033[0;32m'
-reset='\033[0m'
+# warningYellow='\033[0;33m'
+# successGreen='\033[0;32m'
+# reset='\033[0m'
 
-diff=$(git ls-files --modified tests/src/expected)
-if [[ $diff = "" ]]; then
-  printf "${successGreen}✅ No unstaged tests difference.${reset}\n"
-else
-  printf "${warningYellow}⚠️ There are unstaged differences in tests/! Did you break a test?\n${diff}\n${reset}"
-  git --no-pager diff tests/src/expected
-  exit 1
-fi
+# diff=$(git ls-files --modified tests/src/expected)
+# if [[ $diff = "" ]]; then
+#   printf "${successGreen}✅ No unstaged tests difference.${reset}\n"
+# else
+#   printf "${warningYellow}⚠️ There are unstaged differences in tests/! Did you break a test?\n${diff}\n${reset}"
+#   git --no-pager diff tests/src/expected
+#   exit 1
+# fi
+
+./rescript-editor-analysis.exe hover tests/src/Auto.res 2 15
