9a4e549233917ddecc57fdf2e52fede1ec123374
Cheng Lou <chenglou92@gmail.com>
2021-04-29 04:36:54 -0700

    Clean up!

--- analysis/test.sh
+++ analysis/test.sh
@@ -1,33 +1,11 @@
 for file in tests/src/*.{res,resi}; do
   output="$(dirname $file)/expected/$(basename $file).txt"
   ./rescript-editor-analysis.exe test $file &> $output
-  # CI
+  # CI. We use LF, and the CI OCaml fork prints CRLF. Convert.
   if [ "$RUNNER_OS" == "Windows" ]; then
-    echo "sedding..."
     perl -pi -e 's/\r\n/\n/g' -- $output
   fi
 done
-
-echo "cat auto"
-cat -A tests/src/expected/Auto.res.txt
-echo "---"
-echo "sed with regex"
-sed -i "s/\r\n/\n/g" tests/src/expected/Auto.res.txt
-cat -A tests/src/expected/Auto.res.txt
-echo "---"
-echo "sed with empty regex"
-sed -i "" tests/src/expected/Auto.res.txt
-cat -A tests/src/expected/Auto.res.txt
-echo "---"
-echo "perl======"
-perl -pi -e 's/\r\n/\n/g' -- tests/src/expected/Auto.res.txt
-cat -A tests/src/expected/Auto.res.txt
-echo "---"
-# echo "zip unzip"
-# zip -ll tests.zip tests/src/expected/*
-# unzip -ll tests.zip
-# cat -A tests/src/expected/Auto.res.txt
-# echo "---"
 
 warningYellow='\033[0;33m'
 successGreen='\033[0;32m'
@@ -38,6 +16,6 @@ if [[ $diff = "" ]]; then
   printf "${successGreen}✅ No unstaged tests difference.${reset}\n"
 else
   printf "${warningYellow}⚠️ There are unstaged differences in tests/! Did you break a test?\n${diff}\n${reset}"
-  git --no-pager diff --word-diff-regex=. tests/src/expected/Auto.res.txt
+  git --no-pager diff tests/src/expected
   exit 1
 fi
--- analysis/testfile2.ml
+++
@@ -1,6 +0,0 @@
-let content = {|
-  hello
-  how are you
-|}
-
-let () = print_endline content
