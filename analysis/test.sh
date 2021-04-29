699b72daa556fd4a4ff7efc2d6d599ebfbcbff8b
Cheng Lou <chenglou92@gmail.com>
2021-04-29 01:30:29 -0700

    dos2unix in test script

--- analysis/test.sh
+++ analysis/test.sh
@@ -1,9 +1,6 @@
 function exp {
   echo "$(dirname $1)/expected/$(basename $1).txt"
 }
-function exp2 {
-  echo "$(dirname $1)/expected/$(basename $1).2.txt"
-}
 
 echo "cat -A test.sh"
 cat -A tests/src/expected/Auto.res.txt
@@ -12,26 +9,21 @@ echo "git diff test.sh"
 git diff tests/src/expected/Auto.res.txt
 echo "done---------"
 
-echo "doing an echo test"
-
-# node ./checkErrors.js
-
 for file in tests/src/*.{res,resi}; do
-  # ./rescript-editor-analysis.exe test $file &> $(exp $file)
-  cat $(exp $file) &> $(exp2 $file)
-  # CI
-  # if [ "$RUNNER_OS" == "Windows" ]; then
-  #   dos2unix $(exp $file)
-  # fi
+  ./rescript-editor-analysis.exe test $file &> $(exp $file)
 done
 
+# CI
+if [ "$RUNNER_OS" == "Windows" ]; then
+  dos2unix tests/src/expected/*
+fi
+
 echo "cat -A test.sh last"
-cat -A tests/src/expected/Auto.res.2.txt
+cat -A tests/src/expected/Auto.res.txt
 echo "done cat last---------"
 echo "git diff test.sh last"
 diff -u tests/src/expected/Auto.res.txt tests/src/expected/Auto.res.2.txt
 echo "done git last---------"
-
 
 warningYellow='\033[0;33m'
 successGreen='\033[0;32m'
@@ -42,8 +34,6 @@ if [[ $diff = "" ]]; then
   printf "${successGreen}✅ No unstaged tests difference.${reset}\n"
 else
   printf "${warningYellow}⚠️ There are unstaged differences in tests/! Did you break a test?\n${diff}\n${reset}"
-  # node ./checkErrors.js
-
   git --no-pager diff --word-diff-regex=. tests/src/expected/Auto.res.txt
   exit 1
 fi
