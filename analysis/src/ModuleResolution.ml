34aa29c53645810ead9b8a7d5534c8ae9062bc53
Cheng Lou <chenglou92@gmail.com>
2021-04-29 07:44:32 -0700

    Update ModuleResolution.ml

--- analysis/src/ModuleResolution.ml
+++ analysis/src/ModuleResolution.ml
@@ -1,5 +1,5 @@
 let rec resolveNodeModulePath ~startPath name =
   let path = Filename.concat (Filename.concat startPath "node_modules") name in
   if Files.exists path then Some path
-  else if startPath = "/" || Filename.dirname startPath = startPath then None
+  else if Filename.dirname startPath = startPath then None
   else resolveNodeModulePath ~startPath:(Filename.dirname startPath) name
