6969054e8f54e8bb14868eefa6d6bef25b5739eb
Cheng Lou <chenglou92@gmail.com>
2021-05-17 00:58:55 -0700

    Format

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -2,13 +2,13 @@ import * as path from "path";
 import { workspace, ExtensionContext, commands } from "vscode";
 
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
 } from "vscode-languageclient/node";
 
-import * as customCommands from './commands';
+import * as customCommands from "./commands";
 
 let client: LanguageClient;
 
@@ -61,55 +61,55 @@ let client: LanguageClient;
 // });
 
 export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(
-		path.join("server", "out", "server.js")
-	);
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+  // The server is implemented in node
+  let serverModule = context.asAbsolutePath(
+    path.join("server", "out", "server.js")
+  );
+  // The debug options for the server
+  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+  let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	let serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions,
-		},
-	};
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  let serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions,
+    },
+  };
 
-	// Options to control the language client
-	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: "file", language: "rescript" }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
-		},
-	};
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: "file", language: "rescript" }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+  };
 
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		"ReScriptLSP",
-		"ReScript Language Server",
-		serverOptions,
-		clientOptions
-	);
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "ReScriptLSP",
+    "ReScript Language Server",
+    serverOptions,
+    clientOptions
+  );
 
-	// Register custom commands
-	commands.registerCommand('rescript-vscode.create_interface', () => {
-        customCommands.createInterface(client);
-    });
+  // Register custom commands
+  commands.registerCommand("rescript-vscode.create_interface", () => {
+    customCommands.createInterface(client);
+  });
 
-	// Start the client. This will also launch the server
-	client.start();
+  // Start the client. This will also launch the server
+  client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
 }
