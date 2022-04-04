23dc996cfd87656b0b7693df8f00f9d1ad6a7ded
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-04-04 13:07:11 +0200

    revert change now unneeded

--- analysis/src/vendor/res_outcome_printer/res_outcome_printer.ml
+++ analysis/src/vendor/res_outcome_printer/res_outcome_printer.ml
@@ -166,7 +166,7 @@ let printPolyVarIdent txt =
          Doc.group (
            Doc.join ~sep:Doc.line (List.map printOutAttributeDoc attrs)
          );
-         Doc.hardLine;
+         Doc.line;
        ]
 
    let rec collectArrowArgs (outType: Outcometree.out_type) args =
