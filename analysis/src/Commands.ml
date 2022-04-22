3bd76e4ca173a873aeed82be69c65dc5e7c5dd08
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-04-22 08:11:02 +0200

    restore check on formatting

--- analysis/src/Commands.ml
+++ analysis/src/Commands.ml
@@ -220,13 +220,14 @@ let rename ~path ~line ~col ~newName =
 
 let format ~path =
   if Filename.check_suffix path ".res" then
-    let {Res_driver.parsetree = structure; comments} =
+    let {Res_driver.parsetree = structure; comments; diagnostics} =
       Res_driver.parsingEngine.parseImplementation ~forPrinter:true
         ~filename:path
     in
-    (* if List.length diagnostics > 0 then ""
-       else *)
-    Res_printer.printImplementation !Res_cli.ResClflags.width structure comments
+    if List.length diagnostics > 0 then ""
+    else
+      Res_printer.printImplementation !Res_cli.ResClflags.width structure
+        comments
   else if Filename.check_suffix path ".resi" then
     let {Res_driver.parsetree = signature; comments; diagnostics} =
       Res_driver.parsingEngine.parseInterface ~forPrinter:true ~filename:path
