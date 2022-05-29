857cf3d8a75f0812ba39ac074af41bc2f74a2e02
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-05-29 12:56:19 +0200

    Examples: no need to instlal gentype.

--- analysis/reanalyze/examples/deadcode/src/TestImmutableArray.gen.tsx
+++
@@ -1,9 +0,0 @@
-/* TypeScript file generated from TestImmutableArray.res by genType. */
-/* eslint-disable import/first */
-
-
-// @ts-ignore: Implicit any on import
-import * as TestImmutableArrayBS__Es6Import from './TestImmutableArray.bs';
-const TestImmutableArrayBS: any = TestImmutableArrayBS__Es6Import;
-
-export const testImmutableArrayGet: <T1>(arr:ReadonlyArray<T1>) => (null | undefined | T1) = TestImmutableArrayBS.testImmutableArrayGet;
