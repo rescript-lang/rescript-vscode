13c36d11f91186f35f8f2abcb733ed4260b23963
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-05-29 12:56:19 +0200

    Examples: no need to instlal gentype.

--- analysis/reanalyze/examples/deadcode/src/DeadTest.gen.tsx
+++
@@ -1,9 +0,0 @@
-/* TypeScript file generated from DeadTest.res by genType. */
-/* eslint-disable import/first */
-
-
-// @ts-ignore: Implicit any on import
-import * as DeadTestBS__Es6Import from './DeadTest.bs';
-const DeadTestBS: any = DeadTestBS__Es6Import;
-
-export const fortyTwoButExported: number = DeadTestBS.fortyTwoButExported;
