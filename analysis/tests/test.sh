cc729863cca39dfe90b11417d587219ab5c6c024
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-05-31 16:38:32 +0200

    Move test infra inside tests/

--- analysis/tests/test.sh
+++ analysis/tests/test.sh
@@ -1,6 +1,6 @@
-for file in tests/src/*.{res,resi}; do
+for file in src/*.{res,resi}; do
   output="$(dirname $file)/expected/$(basename $file).txt"
-  ./rescript-editor-analysis.exe test $file &> $output
+  ../rescript-editor-analysis.exe test $file &> $output
   # CI. We use LF, and the CI OCaml fork prints CRLF. Convert.
   if [ "$RUNNER_OS" == "Windows" ]; then
     perl -pi -e 's/\r\n/\n/g' -- $output
@@ -11,11 +11,11 @@ warningYellow='\033[0;33m'
 successGreen='\033[0;32m'
 reset='\033[0m'
 
-diff=$(git ls-files --modified tests/src/expected)
+diff=$(git ls-files --modified src/expected)
 if [[ $diff = "" ]]; then
   printf "${successGreen}✅ No unstaged tests difference.${reset}\n"
 else
   printf "${warningYellow}⚠️ There are unstaged differences in tests/! Did you break a test?\n${diff}\n${reset}"
-  git --no-pager diff tests/src/expected
+  git --no-pager diff src/expected
   exit 1
 fi
