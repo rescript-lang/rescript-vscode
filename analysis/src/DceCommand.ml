2bd45caed8e203c2f7453f80ea9c890530abe561
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-06-04 22:33:10 +0200

    cleanup

--- analysis/src/DceCommand.ml
+++ analysis/src/DceCommand.ml
@@ -1,7 +1,5 @@
 let command () =
   Reanalyze.RunConfig.dce ();
-  let shouldRun = true in
-  if shouldRun then (
-    Reanalyze.runAnalysis ~cmtRoot:None;
-    let issues = Reanalyze.Log_.Stats.getSortedIssues () in
-    Printf.printf "kinds of issues:%d\n" (List.length (fst issues)))
+  Reanalyze.runAnalysis ~cmtRoot:None;
+  let issues = Reanalyze.Log_.Stats.getSortedIssues () in
+  Printf.printf "kinds of issues:%d\n" (List.length (fst issues))
