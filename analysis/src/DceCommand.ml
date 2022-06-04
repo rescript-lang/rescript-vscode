1e3af36688a931c8cc59098a73e11e6128c6ec23
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-06-04 22:20:34 +0200

    Don't make dce command pollute tests.

--- analysis/src/DceCommand.ml
+++ analysis/src/DceCommand.ml
@@ -1,6 +1,8 @@
 let command () =
   Reanalyze.cli ();
   Reanalyze.RunConfig.dce ();
-  Reanalyze.runAnalysis ~cmtRoot:None;
-  let issues = Reanalyze.Log_.Stats.getSortedIssues () in
-  Printf.printf "kinds of issues:%d\n" (List.length (fst issues))
+  let shouldRun = false in
+  if shouldRun then (
+    Reanalyze.runAnalysis ~cmtRoot:None;
+    let issues = Reanalyze.Log_.Stats.getSortedIssues () in
+    Printf.printf "kinds of issues:%d\n" (List.length (fst issues)))
