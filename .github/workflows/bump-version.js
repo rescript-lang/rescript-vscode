390f4efc9b230ba4ab27787d1f8199e184d338a3
Florian Hammerschmidt <fh@cca.io>
2022-07-05 23:32:45 +0200

    bump-version: handle case when main version minor gets bumped

--- .github/workflows/bump-version.js
+++ .github/workflows/bump-version.js
@@ -23,6 +23,12 @@ if (semver.minor(latestPublish) === semver.minor(release)) {
 else if (semver.minor(latestPublish) > semver.minor(release)) {
   newVersion = semver.inc(newVersion, "patch", semver.rel);
 }
+// If the main release gets a minor bump but did not get published yet, the package.json version
+// will be higher than the one retrieved from the marketplace, so we need to increment from the main release
+// E.g. package.json gets bumped to 1.5.0 -> 1.6.0
+else if (semver.minor(release) - semver.minor(latestPublish) === 1) {
+  newVersion = semver.inc(release, "minor", semver.rel);
+}
 // Otherwise throw an error, because the pre-release version should always be just one
 // minor higher than the release version.
 else {
