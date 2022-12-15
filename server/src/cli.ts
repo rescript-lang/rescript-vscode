5026760667be4381739d690f7ff438871f9b6ea6
Pedro Castro <aspeddro@gmail.com>
2022-12-15 01:40:52 -0300

    update server cli

--- server/src/cli.ts
+++ server/src/cli.ts
@@ -6,10 +6,12 @@ const args = process.argv.slice(2)
 
 const help = `ReScript Language Server
 
+Usage: rescriptlsp [options?]
+
 Options:
 
---version   Print version
---help      Print help`;
+-v, --version   Print version
+-h, --help      Print help`;
 
 (() => {
   if (args.length === 0) {
@@ -18,9 +20,11 @@ Options:
 
   switch (args[0]) {
     case '--version':
+    case '-v':
       console.log(JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf8' })).version);
       process.exit(0);
     case '--help':
+    case '-h':
       console.log(help);
       process.exit(0);
     default:
