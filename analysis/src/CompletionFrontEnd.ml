dc428dac20d6e466b84b9c104b6f7037e5520c7f
Gabriel Nordeborn <gabbe.nord@gmail.com>
2023-01-01 18:40:51 +0100

    fix

--- analysis/src/CompletionFrontEnd.ml
+++ analysis/src/CompletionFrontEnd.ml
@@ -423,7 +423,7 @@ let completionWithParser1 ~currentFile ~debug ~offset ~path ~posCursor ~text =
     if debug then
       Printf.printf "looking for: %s \n" (Completable.toString (Cpath ctxPath))
   in
-  let unsetLookingForPat = lookingForPat := None in
+  let unsetLookingForPat () = lookingForPat := None in
   let appendNestedPat patternPat =
     match !lookingForPat with
     | Some (Completable.Cpattern ({nested = None} as p)) ->
@@ -476,7 +476,7 @@ let completionWithParser1 ~currentFile ~debug ~offset ~path ~posCursor ~text =
         match exp |> exprToContextPath with
         | None -> ()
         | Some ctxPath -> setLookingForPat ctxPath)
-      | _ -> unsetLookingForPat
+      | _ -> unsetLookingForPat ()
   in
   (* Tracks the path through a pattern for where the cursor is. *)
   let typedCompletionPat (pat : Parsetree.pattern) =
@@ -572,7 +572,7 @@ let completionWithParser1 ~currentFile ~debug ~offset ~path ~posCursor ~text =
   in
   let structure_item (iterator : Ast_iterator.iterator)
       (item : Parsetree.structure_item) =
-    unsetLookingForPat;
+    unsetLookingForPat ();
     let processed = ref false in
     (match item.pstr_desc with
     | Pstr_open {popen_lid} ->
