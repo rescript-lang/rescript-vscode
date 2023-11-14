c355552be9b2af5915e4d7760869a77a42b7945f
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2023-11-14 19:36:07 +0100

    Mark where the stack overflow happens.

--- analysis/src/PrintType.ml
+++ analysis/src/PrintType.ml
@@ -1,7 +1,9 @@
 let printExpr ?(lineWidth = 60) typ =
   Printtyp.reset_names ();
+  try
   Res_doc.toString ~width:lineWidth
-    (Res_outcome_printer.printOutTypeDoc (Printtyp.tree_of_typexp false typ))
+    (Res_outcome_printer.printOutTypeDoc (Printtyp.tree_of_typexp false typ (* HERE *)))
+  with _ -> "Error: could not print type"
 
 let printDecl ?printNameAsIs ~recStatus name decl =
   Printtyp.reset_names ();
