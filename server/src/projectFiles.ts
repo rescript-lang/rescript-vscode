3b1d2a90b6a89285a1a0dd7820d06d80a2359465
Gabriel Nordeborn <gabbe.nord@gmail.com>
2024-06-04 09:50:01 +0200

    avoid recomputing project roots and ReScript versions as much as possible

---
+++ server/src/projectFiles.ts
@@ -0,0 +1,25 @@
+import * as cp from "node:child_process";
+import * as p from "vscode-languageserver-protocol";
+
+export type filesDiagnostics = {
+  [key: string]: p.Diagnostic[];
+};
+
+interface projectFiles {
+  openFiles: Set<string>;
+  filesWithDiagnostics: Set<string>;
+  filesDiagnostics: filesDiagnostics;
+  rescriptVersion: string | undefined;
+
+  bsbWatcherByEditor: null | cp.ChildProcess;
+
+  // This keeps track of whether we've prompted the user to start a build
+  // automatically, if there's no build currently running for the project. We
+  // only want to prompt the user about this once, or it becomes
+  // annoying.
+  // The type `never` means that we won't show the prompt if the project is inside node_modules
+  hasPromptedToStartBuild: boolean | "never";
+}
+
+export let projectsFiles: Map<string, projectFiles> = // project root path
+  new Map();
