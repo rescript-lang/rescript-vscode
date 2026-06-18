a8ca9d864ee372c8c4a6b045a10e947e3638e48d
Pedro Castro <aspeddro@gmail.com>
2026-06-18 11:05:06 -0300

    Support experimental language server

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -23,6 +23,7 @@ import {
   ServerOptions,
   State,
   TransportKind,
+  DidChangeConfigurationNotification
 } from "vscode-languageclient/node";
 
 import * as customCommands from "./commands";
@@ -89,6 +90,75 @@ export function activate(context: ExtensionContext) {
     "ReScript Language Server",
     "rescript",
   );
+
+  const useExperimentalServer = workspace.getConfiguration("rescript").get<boolean>("useExperimentalServer", false);
+
+  function createExperimentalLanguageClient() {
+    const binaryPathFromNodeModules = Uri.joinPath(
+      context.extensionUri,
+      "node_modules",
+      ".bin",
+      "rescript-language-server",
+    ).fsPath;
+
+    const userServerPath = workspace.getConfiguration("rescript").get<string | null>("experimentalServerPath", null);
+
+    const binaryPath = userServerPath ? userServerPath : binaryPathFromNodeModules;
+
+    let serverOptions: ServerOptions = {
+      run: {
+        command: binaryPath,
+        transport: TransportKind.stdio,
+      },
+      debug: {
+        command: binaryPath,
+        transport: TransportKind.stdio,
+      },
+    };
+
+    // Options to control the language client
+    let clientOptions: LanguageClientOptions = {
+      documentSelector: [{ scheme: "file", language: "rescript" }],
+      outputChannel,
+      markdown: {
+        isTrusted: true,
+      },
+    };
+
+    const client = new LanguageClient(
+      "ReScriptLSP",
+      "ReScript Experimental Language Server",
+      serverOptions,
+      clientOptions,
+    );
+
+    // This sets up a listener that, if we're in code analysis mode, triggers
+    // code analysis as the LS server reports that ReScript compilation has
+    // finished. This is needed because code analysis must wait until
+    // compilation has finished, and the most reliable source for that is the LS
+    // server, that already keeps track of when the compiler finishes in order to
+    // other provide fresh diagnostics.
+    context.subscriptions.push(
+      client.onDidChangeState(({ newState }) => {
+        if (newState === State.Running) {
+          context.subscriptions.push(
+            client.onNotification("rescript/compilationFinished", () => {
+              if (inCodeAnalysisState.active === true) {
+                customCommands.codeAnalysisWithReanalyze(
+                  diagnosticsCollection,
+                  diagnosticsResultCodeActions,
+                  outputChannel,
+                  codeAnalysisRunningStatusBarItem,
+                );
+              }
+            }),
+          );
+        }
+      }),
+    );
+
+    return client;
+  }
 
   function createLanguageClient() {
     // The server is implemented in node
@@ -172,7 +242,7 @@ export function activate(context: ExtensionContext) {
   }
 
   // Create the language client and start the client.
-  client = createLanguageClient();
+  client = useExperimentalServer ? createExperimentalLanguageClient(): createLanguageClient();
 
   // Create a custom diagnostics collection, for cases where we want to report
   // diagnostics programatically from inside of the extension. The reason this
@@ -263,7 +333,7 @@ export function activate(context: ExtensionContext) {
       // Compact success display: project label plus a green check emoji
       compilationStatusBarItem.text = `$(check) ReScript: Ok`;
       compilationStatusBarItem.backgroundColor = undefined;
-      compilationStatusBarItem.color = null;
+      compilationStatusBarItem.color = undefined;
       compilationStatusBarItem.command = undefined;
       const projects = successes.map((e) => e.project).join(", ");
       compilationStatusBarItem.tooltip = projects
@@ -333,7 +403,7 @@ export function activate(context: ExtensionContext) {
         const removeAllCodeAction = new CodeAction("Remove all unused in file");
         const edit = new WorkspaceEdit();
         allRemoveActionEdits.forEach((subEdit) => {
-          subEdit.codeAction.edit.entries().forEach(([uri, [textEdit]]) => {
+          subEdit.codeAction.edit?.entries().forEach(([uri, [textEdit]]) => {
             edit.replace(uri, textEdit.range, textEdit.newText);
           });
         });
@@ -361,7 +431,7 @@ export function activate(context: ExtensionContext) {
 
       const document = editor.document;
       const diagnostics = diagnosticsCollection.get(document.uri);
-      const newDiagnostics = diagnostics.filter((d) => d !== diagnostic);
+      const newDiagnostics = diagnostics?.filter((d) => d !== diagnostic);
       diagnosticsCollection.set(document.uri, newDiagnostics);
     },
   );
@@ -513,7 +583,7 @@ export function activate(context: ExtensionContext) {
 
   commands.registerCommand("rescript-vscode.restart_language_server", () => {
     client.stop().then(() => {
-      client = createLanguageClient();
+      client = useExperimentalServer ? createExperimentalLanguageClient(): createLanguageClient();;
       client.start();
     });
   });
@@ -548,8 +618,12 @@ export function activate(context: ExtensionContext) {
         }
         // Send a general message that configuration has updated. Clients
         // interested can then pull the new configuration as they see fit.
+        // The spec say params DidChangeConfigurationParams can contain the settings fields
+        // https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#didChangeConfigurationParams
         client
-          .sendNotification("workspace/didChangeConfiguration")
+          .sendNotification(DidChangeConfigurationNotification.type, {
+            settings: null,
+          })
           .catch((err) => {
             window.showErrorMessage(String(err));
           });
